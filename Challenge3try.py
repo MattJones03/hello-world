1254157eb0104d8c9137798a6d8c90f9f58c74f2
Matt Jones <matthew@jones03.co.uk>
2018-11-08 04:25:25 -0500

    Solution_Files

--- Challenge3try.py
+++ Challenge3try.py
@@ -1,25 +1,21 @@
-#  Python for Network Engineers Challenge Lab #3 Solution
-#  Revision May 22 2018
-
-#  ASAv add a Network Object named Development with 10.1.1.1
 import requests
+import json
 
 url = "https://192.168.10.100/api/objects/networkobjects"
 
-payload = "{\r\n  \"host\": {\r\n    \"kind\": \"IPv4Address\",\r\n    \"value\": \"100.1.1.1\"\r\n  },\r\n  \"kind\": \"object#NetworkObj\",\r\n  \"name\": \"Development\",\r\n  \"objectId\": \"Development\"\r\n}\r\n\r\n\r\n\r\n\r\n"
+payload = "{\r\n  \"host\": {\r\n    \"kind\": \"IPv4Address\",\r\n    \"value\": \"100.1.1.1\"\r\n  },\r\n  \"kind\": \"object#NetworkObj\",\r\n  \"name\": \"Development\",\r\n  \"objectId\": \"Development\"\r\n}"
 headers = {
     'Content-Type': "application/json",
     'Authorization': "Basic ZW5hYmxlXzE6Y2lzY28="
     }
 
-response = requests.request("POST", url, data=payload, verify=False, headers=headers)
+response = requests.request("POST", url, verify=False, data=payload, headers=headers)
 
 print(response.text)
 
-#  NXOSv add vlans 600 and 700
-
-import json
-
+"""
+Modify these please
+"""
 url='http://192.168.10.60/ins'
 switchuser='admin'
 switchpassword='Passw0rd1'
@@ -37,11 +33,9 @@ payload={
 }
 response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
 
-# IOS XE add static route
-
 url = "http://192.168.10.80/restconf/api/config/native/ip/route"
 
-payload = "{\r\n\t\"ned:route\": {\r\n\t\t\"ip-route-interface-forwarding-list\": [{\r\n\t\t\t\"prefix\": \"216.48.1.0\",\r\n\t\t\t\"mask\": \"255.255.255.0\",\r\n\t\t\t\"fwd-list\": [{\r\n\t\t\t\t\"fwd\": \"10.1.1.1\"\r\n\t\t\t}]\r\n\t\t}]\r\n\t}\r\n}"
+payload = "{\r\n    \"ned:route\": {\r\n        \"ip-route-interface-forwarding-list\": [\r\n                        {\r\n                \"prefix\": \"216.48.1.0\",\r\n                \"mask\": \"255.255.255.0\",\r\n                \"fwd-list\": [\r\n                    {\r\n                        \"fwd\": \"10.1.1.1\"\r\n                    }\r\n                ]\r\n            }\r\n        ],\r\n        \"static\": {}\r\n    }\r\n}"
 headers = {
     'Content-Type': "application/vnd.yang.data+json",
     'Accept': "application/vnd.yang.data+json",
